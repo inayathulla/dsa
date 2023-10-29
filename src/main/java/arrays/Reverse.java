e40171d4d59bf202994cad5721c5183a2e8f5c82
inayathulla <inayathulla2020@gmail.com>
2023-10-29 12:09:54 +0530

    Better way to reverse

--- src/main/java/arrays/Reverse.java
+++ src/main/java/arrays/Reverse.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class Reverse {
     public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        int[] arr = {10, 20, 30, 40, 50, 60, 70, 80};
         int start = 0;
         int end = arr.length -1;
         int temp;
-        while(true){
+/*        while(true){
             if (start == end) {
                 break;
             } else if (start < end) {
@@ -18,7 +18,23 @@ public class Reverse {
             }
             start++;
             end--;
+        }*/
+
+        // Better way
+        while (true){
+            if (start < end) {
+                swap(arr, start, end);
+                start++;
+                end--;
+            } else {
+                break;
+            }
         }
         System.out.println(Arrays.toString(arr));
+    }
+    private static void swap(int[] arr, int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 }
