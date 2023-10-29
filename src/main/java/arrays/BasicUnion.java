14962413f73e37a52951d7a313aa3c5fc82110e0
inayathulla <inayathulla2020@gmail.com>
2023-10-29 16:27:10 +0530

    Basic union with vector.

---
+++ src/main/java/arrays/BasicUnion.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class BasicUnion {
+    public static void main(String[] args) {
+        int[] arr = {1,4,7,8};
+        int[] brr = {2,9};
+        Vector<Integer> result = new Vector<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            result.add(arr[i]);
+        }
+
+        for (int i = 0; i <brr.length ; i++) {
+            result.add(brr[i]);
+        }
+
+        System.out.println("Elements after union are: "+result);
+    }
+}
