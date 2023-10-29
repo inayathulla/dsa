aaf36c76737c60552cd10c64ae6877362b92bde2
inayathulla <inayathulla2020@gmail.com>
2023-10-29 16:27:42 +0530

    Basic union with vector.

--- src/main/java/arrays/BasicUnion.java
+++ src/main/java/arrays/BasicUnion.java
@@ -9,7 +9,7 @@ public class BasicUnion {
     public static void main(String[] args) {
         int[] arr = {1,4,7,8};
         int[] brr = {2,9};
-        Vector<Integer> result = new Vector<>();
+        Vector<Integer> result = new Vector<>(); // Can grow size unlike array
 
         for (int i = 0; i < arr.length; i++) {
             result.add(arr[i]);
